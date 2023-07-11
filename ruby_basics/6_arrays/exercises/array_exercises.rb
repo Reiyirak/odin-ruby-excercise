0997872c6785cc20c9e017fcbf6b45ba9c74d99c
Reiyirak <pdsmej@gmail.com>
2023-07-10 21:22:16 -0600

    Finished first two excercises for arrays, break time to fix nvim

--- ruby_basics/6_arrays/exercises/array_exercises.rb
+++ ruby_basics/6_arrays/exercises/array_exercises.rb
@@ -1,10 +1,11 @@
 def nil_array(number)
   # return an array containing `nil` the given number of times
-  number = [nil]
+  Array.new(number, nil)
 end
 
 def first_element(array)
   # return the first element of the array
+  array.first()
 end
 
 def third_element(array)
